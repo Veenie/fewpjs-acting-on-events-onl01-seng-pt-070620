de2ba098eff07d51889a1461eabb4a01016cc0b4
Raveen K Sharma <raveen08@gmail.com>
2021-01-15 19:21:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ dodger.style.backgroundColor = "#FF69B4";
 
 dodger.style.bottom = "0px";
 
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+ 
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
