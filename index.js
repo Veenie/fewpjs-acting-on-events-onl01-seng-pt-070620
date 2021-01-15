c83f378bf5e23892e86eabe33ef195acd8c8228b
Raveen K Sharma <raveen08@gmail.com>
2021-01-15 19:16:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,4 @@ let dodger = document.getElementById("dodger");
 dodger.style.backgroundColor = "#FF69B4";
 
 dodger.style.bottom = "0px";
-document.addEventListener("keydown", function(e) {
-  console.log(e.key);
-});
+
